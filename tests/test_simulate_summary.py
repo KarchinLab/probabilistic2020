a0e6ef35ad142964cc46a0ec9be599bff545f701
ctokheim <collintokheim@gmail.com>
2014-12-25 18:43:17 -0500

    Added unit test for simulate_summary script

---
+++ tests/test_simulate_summary.py
@@ -0,0 +1,26 @@
+# fix problems with pythons terrible import system
+import os
+import sys
+file_dir = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(file_dir, '../bin/'))
+
+import simulate_summary as sm
+
+def test_sim_summary():
+    opts = {'input': os.path.join(file_dir, 'data/sim_summary.fa'),
+            'mutations': os.path.join(file_dir, 'data/sim_summary_mutations.txt'),
+            'bed': os.path.join(file_dir, 'data/sim_summary.bed'),
+            'processes': 0,
+            'num_permutations': 1,
+            'context': 1.5,
+            'summary': False,
+            'maf': True,
+            'use_unmapped': False,
+            'genome': '',
+            'output': os.path.join(file_dir, 'output/sim_summary.txt')
+            }
+
+    sm.main(opts)
+
+if __name__ == '__main__':
+    test_sim_summary()
